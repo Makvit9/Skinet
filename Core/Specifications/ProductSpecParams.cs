9dcbc2f25f2e1f98d9f69d79abec51772538a474
Makvit9 <crynged9@gmail.com>
2025-04-26 21:42:38 +0300

    Adding  Search Functionality

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -47,4 +47,13 @@ public class ProductSpecParams
 
 
     public string? Sort {get; set;}
+
+    private string? _search;
+    public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }
+    
+
 }
