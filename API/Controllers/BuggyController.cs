119d5d5d8472ca8a0cf29e8ca57751ebf9277847
Makvit9 <crynged9@gmail.com>
2025-04-27 15:16:32 +0300

    Adding Exception Handling Middleware

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,47 @@
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController :BaseController
+{
+    [HttpGet("unauthorized")]
+
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+
+    [HttpGet("badrequest")]
+
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Bad Request!");
+    }
+
+     [HttpGet("notfound")]
+
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+
+     [HttpGet("internalerror")]
+
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("This is for internal exceptions");
+    }
+
+    [HttpPost("validationerror")]
+
+    public IActionResult GetValidationError(Product product)
+    {
+        return Ok();
+    }
+
+
+
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -21,7 +22,7 @@ builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.MapControllers();
 
 try
