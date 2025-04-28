751f036e23d41165a6f8307716d8b37b2f68484d
Makvit9 <crynged9@gmail.com>
2025-04-29 02:56:24 +0300

    Creating HTTP request in App component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,23 @@ import { HeaderComponent } from "./layout/header/header.component";
   styleUrl: './app.component.scss',
   imports: [HeaderComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'client';
+
+  products: any[] = [];
+
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data ,
+      error: error => console.log(error),
+      complete: () => console.log('completed')
+    })
+
+  }
+
+
 
 }
