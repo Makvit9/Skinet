973148c484082dd61f59c89fe96d8708172ed074
Makvit9 <crynged9@gmail.com>
2025-04-29 01:52:05 +0300

    Creating the Header of Client App

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'client';
