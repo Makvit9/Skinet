0f405de2b379555c7b6f2f9304274d1278b46c8b
Makvit9 <crynged9@gmail.com>
2025-04-29 17:13:03 +0300

    Craeting Shop Component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,31 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component  } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Pagination } from './shared/models/pagination';
-import { Product } from './shared/models/product';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, ShopComponent]
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  title = 'client';
+export class AppComponent  {
 
-  products: Product[] = [];
-
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next:  response => this.products = response.data ,
-      error: error => console.log(error),
-      complete: () => console.log('completed')
-    })
-
-  }
+  
 
 
 
