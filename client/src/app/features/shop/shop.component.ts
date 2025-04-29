a9f1e2cea8b62f5e1325b6c5a1a5dcf829dd1a04
Makvit9 <crynged9@gmail.com>
2025-04-29 19:27:10 +0300

    Adding Filtering to the Project

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -2,10 +2,16 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/product';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-shop',
   imports: [
-    ProductItemComponent
+    ProductItemComponent,
+    MatButton,
+    MatIcon
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
@@ -13,17 +19,49 @@ import { ProductItemComponent } from "./product-item/product-item.component";
 export class ShopComponent implements OnInit{
  
   private shopService = inject(ShopService);
-  title = 'client';
-
+  private dialogService = inject(MatDialog);
   products: Product[] = [];
 
+  selectedBrands: string[] = [];
+  selectedTypes: string[] = [];
 
   ngOnInit(): void {
+    
+    this.initializeShop();
+  }
+
+  initializeShop() {
+
+    this.shopService.getBrands();
+    this.shopService.getTypes();
     this.shopService.getProducts().subscribe({
       next:  response => this.products = response.data ,
       error: error => console.log(error)
-    })
-
+    });
   }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data: {
+        selectedBrands: this.selectedBrands,
+        selectedTypes: this.selectedTypes
+      }
+    });
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if (result) {
+          this.selectedBrands = result.selectedBrands;
+          this.selectedTypes = result.selectedTypes;
+          this.shopService.getProducts(this.selectedBrands,this.selectedTypes).subscribe({
+            next: response => this.products = response.data,
+            error: error => console.log(error)
+            
+          })
+        }
+      }
+    })
+  }
+
 
 }
