9df1f7c0f46ceb7ff64cb03030e4384c38890bb9
Makvit9 <crynged9@gmail.com>
2025-05-02 15:59:54 +0300

    Creating Cart Classes

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
@@ -19,6 +20,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 builder.Services.AddCors();
+
 builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 {
     var connString = builder.Configuration.GetConnectionString("Redis") ??
@@ -27,6 +29,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
     var configuration = ConfigurationOptions.Parse(connString,true);
     return ConnectionMultiplexer.Connect(configuration);
 });
+builder.Services.AddSingleton<ICartService, CartService>();
 
 
 var app = builder.Build();
