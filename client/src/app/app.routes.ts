9c50536401eefb4e9163b68f577857fbaa5c78f5
Makvit9 <crynged9@gmail.com>
2025-05-02 13:01:03 +0300

    Adding the route for not-found and server-error comoponents

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 import { TestErrorComponent } from './features/test-error/test-error.component';
+import { ServerErrorComponent } from './share/components/server-error/server-error.component';
+import { NotFoundComponent } from './share/components/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component:ProductDetailsComponent },
     {path: 'test-error', component:TestErrorComponent },
-    {path: '**', redirectTo: '', pathMatch:'full' },
+    {path: 'not-found', component:NotFoundComponent },
+    {path: 'server-error', component:ServerErrorComponent },
+    {path: '**', redirectTo: 'not-found', pathMatch:'full' },
 ];
